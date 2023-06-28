35805812635e55bccfaa4b5b50870ef821e24777
Adriel <adriel.fsantos@outlook.com>
2023-06-28 13:52:48 -0300

    Adiciona função permutar

--- main.c
+++ main.c
@@ -1,17 +1,8 @@
 #include <stdio.h>
 
-unsigned long long permutacaoInicial(unsigned long long entrada)
+unsigned long long permutar(unsigned long long entrada, int tabelaPermutacao[64])
 {
   unsigned long long saida = 0;
-  int tabelaPermutacao[64] = {
-      58, 50, 42, 34, 26, 18, 10, 2,
-      60, 52, 44, 36, 28, 20, 12, 4,
-      62, 54, 46, 38, 30, 22, 14, 6,
-      64, 56, 48, 40, 32, 24, 16, 8,
-      57, 49, 41, 33, 25, 17, 9, 1,
-      59, 51, 43, 35, 27, 19, 11, 3,
-      61, 53, 45, 37, 29, 21, 13, 5,
-      63, 55, 47, 39, 31, 23, 15, 7};
 
   // percorre os 64 bits fazendo a permutação
   for (int i = 0; i < 64; i++)
@@ -26,6 +17,20 @@ unsigned long long permutacaoInicial(unsigned long long entrada)
   }
 
   return saida;
+}
+
+unsigned long long permutacaoInicial(unsigned long long entrada)
+{
+  int tabelaPermutacao[64] = {
+      58, 50, 42, 34, 26, 18, 10, 2,
+      60, 52, 44, 36, 28, 20, 12, 4,
+      62, 54, 46, 38, 30, 22, 14, 6,
+      64, 56, 48, 40, 32, 24, 16, 8,
+      57, 49, 41, 33, 25, 17, 9, 1,
+      59, 51, 43, 35, 27, 19, 11, 3,
+      61, 53, 45, 37, 29, 21, 13, 5,
+      63, 55, 47, 39, 31, 23, 15, 7};
+  return permutar(entrada, tabelaPermutacao);
 }
 
 int main()
