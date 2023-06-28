a95d46cfe2c00086f778b45b31a7914b7e2bef50
Adriel <adriel.fsantos@outlook.com>
2023-06-28 13:55:38 -0300

    Adiciona função de permutação inversa

--- main.c
+++ main.c
@@ -33,14 +33,30 @@ unsigned long long permutacaoInicial(unsigned long long entrada)
   return permutar(entrada, tabelaPermutacao);
 }
 
+unsigned long long permutacaoInversa(unsigned long long entrada)
+{
+  int tabelaPermutacao[64] = {
+      40, 8, 48, 16, 56, 24, 64, 32,
+      39, 7, 47, 15, 55, 23, 63, 31,
+      38, 6, 46, 14, 54, 22, 62, 30,
+      37, 5, 45, 13, 53, 21, 61, 29,
+      36, 4, 44, 12, 52, 20, 60, 28,
+      35, 3, 43, 11, 51, 19, 59, 27,
+      34, 2, 42, 10, 50, 18, 58, 26,
+      33, 1, 41, 9, 49, 17, 57, 25};
+  return permutar(entrada, tabelaPermutacao);
+}
+
 int main()
 {
   // testando funcao
   unsigned long long input = 0x8000000000000000; // Exemplo de entrada de 64 bits
   unsigned long long output = permutacaoInicial(input);
+  unsigned long long inverso = permutacaoInversa(output);
 
   printf("Input:  %016llX\n", input);
   printf("Output: %016llX\n", output);
+  printf("inverso: %016llX\n", inverso);
 
   return 0;
 }
