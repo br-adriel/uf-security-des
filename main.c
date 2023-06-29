ee2aec378e3d08f75c48807354a88b46964edfff
Adriel <adriel.fsantos@outlook.com>
2023-06-29 00:05:26 -0300

    Adiciona função geradora da chave de 56 bits

--- main.c
+++ main.c
@@ -1,24 +1,31 @@
 #include <stdio.h>
 
-unsigned long long permutar(unsigned long long entrada, int tabelaPermutacao[64])
+/**
+ * Permuta a entrada de acordo com a tabelaPermutacao fornecida, retonando uma
+ * saída com tamanhoPermutacao bits
+ */
+unsigned long long permutar(unsigned long long entrada, int tabelaPermutacao[], int tamanhoPermutacao)
 {
   unsigned long long saida = 0;
 
   // percorre os 64 bits fazendo a permutação
-  for (int i = 0; i < 64; i++)
+  for (int i = 0; i < tamanhoPermutacao; i++)
   {
     // move o bit da posição tabelaPermutacao[i] para o inicio do numero e o
     // "seleciona" através de um OU com um 1 apenas na última posição
-    int bitsDeslocamento = 64 - tabelaPermutacao[i];
+    int bitsDeslocamento = tamanhoPermutacao - tabelaPermutacao[i];
     unsigned long long bit = (entrada >> bitsDeslocamento) & 1ULL;
 
     // insere o bit na saida na posicao indicada pelo indice da tabelaPermutacao
-    saida |= (bit << (63 - i));
+    saida |= (bit << (tamanhoPermutacao - 1 - i));
   }
 
   return saida;
 }
 
+/**
+ * Realiza a permutação inicial do algoritmo
+ */
 unsigned long long permutacaoInicial(unsigned long long entrada)
 {
   int tabelaPermutacao[64] = {
@@ -30,9 +37,12 @@ unsigned long long permutacaoInicial(unsigned long long entrada)
       59, 51, 43, 35, 27, 19, 11, 3,
       61, 53, 45, 37, 29, 21, 13, 5,
       63, 55, 47, 39, 31, 23, 15, 7};
-  return permutar(entrada, tabelaPermutacao);
+  return permutar(entrada, tabelaPermutacao, 64);
 }
 
+/**
+ * Realiza a permutação final do algoritmo
+ */
 unsigned long long permutacaoInversa(unsigned long long entrada)
 {
   int tabelaPermutacao[64] = {
@@ -44,7 +54,29 @@ unsigned long long permutacaoInversa(unsigned long long entrada)
       35, 3, 43, 11, 51, 19, 59, 27,
       34, 2, 42, 10, 50, 18, 58, 26,
       33, 1, 41, 9, 49, 17, 57, 25};
-  return permutar(entrada, tabelaPermutacao);
+  return permutar(entrada, tabelaPermutacao, 64);
+}
+
+/**
+ * Gera a chave de 56 bits usada para gerar as sub-chaves
+ */
+unsigned long long gerarChave56(unsigned long long chavePrincipal)
+{
+  // remove bits na oitava posição - (bits de paridade)
+  unsigned long long mascara = 0xFEFEFEFEFEFEFEFEULL;
+  unsigned long long chave56 = chavePrincipal & mascara;
+
+  // permutacao da chave
+  int tabelaSubchave[56] = {
+      57, 49, 41, 33, 25, 17, 9, 1,
+      58, 50, 42, 34, 26, 18, 10, 2,
+      59, 51, 43, 35, 27, 19, 11, 3,
+      60, 52, 44, 36, 63, 55, 47, 39,
+      31, 23, 15, 7, 62, 54, 46, 38,
+      30, 22, 14, 6, 61, 53, 45, 37,
+      29, 21, 13, 5, 28, 20, 12, 4};
+  unsigned long long subchave = permutar(chave56, tabelaSubchave, 56);
+  return subchave;
 }
 
 int main()
@@ -53,10 +85,12 @@ int main()
   unsigned long long input = 0x8000000000000000; // Exemplo de entrada de 64 bits
   unsigned long long output = permutacaoInicial(input);
   unsigned long long inverso = permutacaoInversa(output);
+  unsigned long long subchave = gerarSubchaves(0x123456789ABCDEF0);
 
   printf("Input:  %016llX\n", input);
   printf("Output: %016llX\n", output);
   printf("inverso: %016llX\n", inverso);
+  printf("subchave: %00llX\n", subchave);
 
   return 0;
 }
